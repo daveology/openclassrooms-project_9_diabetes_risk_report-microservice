23f7c765d1855d746062cebcfb794e211c7f859e
David <cheery.spot1750@fastmail.com>
2022-01-01 17:54:09 +0100

    feat: add triggersCount

--- src/main/java/com/mediscreen/service/ReportService.java
+++ src/main/java/com/mediscreen/service/ReportService.java
@@ -3,29 +3,36 @@ package com.mediscreen.service;
 import com.mediscreen.model.Note;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 @Service
 public class ReportService {
 
     public String generateReport(Collection<Note> noteList) {
 
-        Set<String> triggers = new HashSet<>(Arrays.asList(
-                "Hémoglobine A1C",
-                "Microalbumine",
-                "Taille",
-                "Poids",
-                "Fumeur",
-                "Anormal",
-                "Cholestérol",
-                "Vertige",
-                "Rechute",
-                "Réaction",
-                "Anticorps"
-        ));
+        HashMap<String, Boolean> triggers = new HashMap<>();
+        triggers.put("Hémoglobine A1C", false);
+        triggers.put("Microalbumine A1C", false);
+        triggers.put("Taille A1C", false);
+        triggers.put("Poids", false);
+        triggers.put("Fumeur", false);
+        triggers.put("Anormal", false);
+        triggers.put("Cholestérol", false);
+        triggers.put("Vertige", false);
+        triggers.put("Rechute", false);
+        triggers.put("Réaction", false);
+        triggers.put("Anticorps", false);
+
+        noteList.stream().forEach(note -> {
+            triggers.forEach((key, value) -> {
+                if (note.getContent().contains(key)) { value = true; }
+            });
+        });
+
+        Long triggersCount = triggers.entrySet()
+                .stream()
+                .filter(entry -> entry.getValue() == true)
+                .count();
 
         return "";
     }
