16064758857e5ecb08700af5f901a6c1b0692447
David <cheery.spot1750@fastmail.com>
2022-01-01 18:10:55 +0100

    feat: add EARLY ON SET case

--- src/main/java/com/mediscreen/service/ReportService.java
+++ src/main/java/com/mediscreen/service/ReportService.java
@@ -1,6 +1,7 @@
 package com.mediscreen.service;
 
 import com.mediscreen.model.Note;
+import com.mediscreen.model.Report;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -11,10 +12,15 @@ public class ReportService {
     public String generateReport(int age, String gender, Collection<Note> noteList) {
 
         Long triggersCount = getTriggersCount(noteList);
+        String riskLevel = "NONE";
 
+        if (age > 30 && triggersCount >= 8L
+        || gender == "M" && age < 30 && triggersCount >= 5L
+        || gender == "F" && age < 30 && triggersCount >= 7L) {
+            riskLevel = Report.EARLY_ONSET.toString();
+        }
 
-
-        return "";
+        return riskLevel;
     }
 
     public Long getTriggersCount(Collection<Note> noteList) {
