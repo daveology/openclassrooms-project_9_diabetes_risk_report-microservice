102f96a020262cf782c18f4b6a767d9778b422e5
David <cheery.spot1750@fastmail.com>
2022-01-01 17:29:10 +0100

    feat: add triggers set of String

--- src/main/java/com/mediscreen/service/ReportService.java
+++ src/main/java/com/mediscreen/service/ReportService.java
@@ -3,12 +3,29 @@ package com.mediscreen.service;
 import com.mediscreen.model.Note;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 public class ReportService {
 
     public String generateReport(Collection<Note> noteList) {
+
+        Set<String> triggers = new HashSet<>(Arrays.asList(
+                "Hémoglobine A1C",
+                "Microalbumine",
+                "Taille",
+                "Poids",
+                "Fumeur",
+                "Anormal",
+                "Cholestérol",
+                "Vertige",
+                "Rechute",
+                "Réaction",
+                "Anticorps"
+        ));
 
         return "";
     }
