67dfd747ff778aa02cb46a286328b7c27a730603
David <cheery.spot1750@fastmail.com>
2022-01-01 16:53:32 +0100

    build: add base Spring Boot project structure

---
+++ src/main/java/com/mediscreen/Openclassrooms9thProjectReportMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.mediscreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Openclassrooms9thProjectReportMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Openclassrooms9thProjectReportMicroserviceApplication.class, args);
+    }
+
+}
