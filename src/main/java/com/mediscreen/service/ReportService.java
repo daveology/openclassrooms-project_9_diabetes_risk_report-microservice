c3aa12d864d38cd308a6b2a3e8e6b39259485e10
David <cheery.spot1750@fastmail.com>
2022-01-01 17:15:28 +0100

    feat: add generateReport service method

--- src/main/java/com/mediscreen/service/ReportService.java
+++ src/main/java/com/mediscreen/service/ReportService.java
@@ -1,9 +1,15 @@
 package com.mediscreen.service;
 
+import com.mediscreen.model.Note;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
 
 @Service
 public class ReportService {
 
+    public String generateReport(Collection<Note> noteList) {
 
+        return "";
+    }
 }
