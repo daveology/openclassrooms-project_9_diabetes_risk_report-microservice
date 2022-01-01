d0b260fea27961aeb9cce332be351f3a34039c1f
David <cheery.spot1750@fastmail.com>
2022-01-01 17:11:32 +0100

    feat: add generateReport controller's endpoint

--- src/main/java/com/mediscreen/controller/ReportController.java
+++ src/main/java/com/mediscreen/controller/ReportController.java
@@ -1,9 +1,16 @@
 package com.mediscreen.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ReportController {
 
+    @GetMapping("/generateReport")
+    public String generateReport(@RequestBody Collection<Note> noteList) {
 
+        return patientReadService.generateReport(noteList);
+    }
 }
