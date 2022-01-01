8a99312a6d3a9e3d8261fc2dc8b3769e1d89e5ac
David <cheery.spot1750@fastmail.com>
2022-01-01 17:16:07 +0100

    feat: instanciate ReportService into ReportController

--- src/main/java/com/mediscreen/controller/ReportController.java
+++ src/main/java/com/mediscreen/controller/ReportController.java
@@ -1,6 +1,8 @@
 package com.mediscreen.controller;
 
 import com.mediscreen.model.Note;
+import com.mediscreen.service.ReportService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +12,12 @@ import java.util.Collection;
 @RestController
 public class ReportController {
 
+    @Autowired
+    private ReportService reportService;
+
     @GetMapping("/generateReport")
     public String generateReport(@RequestBody Collection<Note> noteList) {
 
-        return patientReadService.generateReport(noteList);
+        return reportService.generateReport(noteList);
     }
 }
