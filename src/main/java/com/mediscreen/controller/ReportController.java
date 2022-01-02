3697472dd47b3a94aec9fbd00f0bbfb9ccc5ef9b
David <cheery.spot1750@fastmail.com>
2022-01-02 19:05:26 +0100

    fix: update generateReport endpoint

--- src/main/java/com/mediscreen/controller/ReportController.java
+++ src/main/java/com/mediscreen/controller/ReportController.java
@@ -1,11 +1,15 @@
 package com.mediscreen.controller;
 
 import com.mediscreen.dto.ReportEntriesDto;
+import com.mediscreen.model.Note;
 import com.mediscreen.service.ReportService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 @RestController
 public class ReportController {
@@ -16,6 +20,9 @@ public class ReportController {
     @PostMapping("/generateReport")
     public String generateReport(@RequestBody ReportEntriesDto entries) {
 
-        return reportService.generateReport(entries.getAge(), entries.getGender(), entries.getNoteList());
+        Collection<Note> noteList = new ArrayList<>();
+        if (noteList != null) {noteList.addAll(noteList);}
+        String result = reportService.generateReport(entries.getAge(), entries.getGender(), noteList);
+        return result;
     }
 }
