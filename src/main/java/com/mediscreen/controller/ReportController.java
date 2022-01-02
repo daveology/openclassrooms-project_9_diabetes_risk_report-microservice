25a367f95a4c7999b9a062efcad0908b1e489bdb
David <cheery.spot1750@fastmail.com>
2022-01-02 16:55:25 +0100

    build: add dto directory and ReportEntriesDto model

--- src/main/java/com/mediscreen/controller/ReportController.java
+++ src/main/java/com/mediscreen/controller/ReportController.java
@@ -1,13 +1,11 @@
 package com.mediscreen.controller;
 
-import com.mediscreen.model.Note;
+import com.mediscreen.dto.ReportEntriesDto;
 import com.mediscreen.service.ReportService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Collection;
 
 @RestController
 public class ReportController {
